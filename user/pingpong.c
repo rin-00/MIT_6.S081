a3642f990bb75883ae788491186b26e0ea8f0455
jie <2403670885@qq.com>
2022-07-14 02:18:05 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -9,12 +9,12 @@ int main(int argc,char *argv[])
 	int p1[2];
 	int p2[2]; 
 	pipe(p1); 
-	pipe(p2);    //建立两个管道
+	pipe(p2);    
 	
 	int pid;
-	pid=fork();  //产生子进程
+	pid=fork();  
 	
-	char buf[1];  //传输数据的缓存区,大小为1字节 
+	char buf[1];  
 	
 	if(pid<0)
 	{ //子进程生成失败
@@ -23,23 +23,23 @@ int main(int argc,char *argv[])
 	else if(pid==0)
 	{//子进程
 		close(p1[WRITE]);
-		close(p2[READ]);   //关闭未使用的管道 
-		read(p1[READ],buf,1);  //子进程从p1管道读
+		close(p2[READ]);   
+		read(p1[READ],buf,1); 
 		printf("%d:received ping\n",getpid());
-		write(p2[WRITE]," ",1);//子进程从p2管道写
+		write(p2[WRITE],"q",1);
 		close(p1[READ]);
-		close(p2[WRITE]);  //关闭所有管道
+		close(p2[WRITE]); 
 		exit(0); 
 	} 
 	else
 	{//父进程 
 		close(p1[READ]);
-		close(p2[WRITE]);  //关闭未使用的管道
-		write(p1[WRITE]," ",1);  //父进程从p1管道写
-		read(p2[READ],buf,1);   //父进程从p2管道读
+		close(p2[WRITE]);  
+		write(p1[WRITE],"p",1);  
+		read(p2[READ],buf,1);  
 		printf("%d:received pong\n",getpid()); 
 		close(p1[WRITE]);
-		close(p2[READ]);   //关闭所有管道 
+		close(p2[READ]);  
 		exit(0);		  	
 	}
 }
