4b31778cb39dc99ee8c60a7d5f3dd7ba758f6134
jie <2403670885@qq.com>
2022-07-18 01:07:12 +0800

    xargs

--- user/xargs.c
+++ user/xargs.c
@@ -7,44 +7,51 @@ int main(int argc, char* argv[])
 {
 	char* command = argv[1];
 	char paramv[MAXARG][MAX_LEN];   //max argument 
-	memset(paramv, 0, MAXARG * MAX_LEN); //初始化 
-	for (int i = 1; i < argc; i++)
-	{//存储指令 
-		strcpy(paramv[i - 1], argv[i]);
-	}
+
 	char* p[MAXARG];
-	int read_result;
-	char buf;
-	int count = argc - 1;
-	int tag = 0;
-	int pos = 0; //语句中字符位置 
-	while ((read_result = read(0, &buf, 1)) > 0 && buf != '\n')
+	while (1)
 	{
-		if (buf == ' ' && tag == 1)
-		{//下一条语句 
-			pos = 0;
-			tag = 0;
-			count++;
+		int read_result;
+		char buf;
+		int count = argc - 1;
+		int flag = 0;
+		int pos = 0; //语句中字符位置 	
+
+		memset(paramv, 0, MAXARG * MAX_LEN); //初始化 
+		for (int i = 1; i < argc; i++)
+		{//存储指令 
+			strcpy(paramv[i - 1], argv[i]);
 		}
-		else if (buf != ' ')
+
+		while ((read_result = read(0, &buf, 1)) > 0 && buf != '\n')
 		{
-			paramv[count][pos++] = buf;
-			tag = 1;
+			if (buf == ' ' && flag == 1)
+			{//下一条语句，重置标识
+				pos = 0;
+				flag = 0;
+				count++;
+			}
+			else if (buf != ' ')
+			{
+				paramv[count][pos++] = buf;
+				flag = 1;
+			}
 		}
-	}
-	if (read_result <= 0)
-		break;
-	for (int i = 0; i <= MAXARG; i++)
-		p[i] = paramv[i];
-	p[MAXARG - 1] = 0;
-	if (fork() == 0)
-	{//子进程 
-		exec(command, p);
-		exit(0);
-	}
-	else
-	{//父进程 
-		wait((int*)0); //等待子进程完成 
+		//当读到EOF或\n时，结束
+		if (read_result <= 0)
+			break;  
+		for (int i = 0; i < MAXARG - 1; i++)
+			p[i] = paramv[i];
+		p[MAXARG - 1] = 0;
+		if (fork() == 0)
+		{//子进程 
+			exec(command, p);
+			exit(0);
+		}
+		else
+		{//父进程 
+			wait((int*)0); //等待子进程完成 
+		}
 	}
 	exit(0);
 }
